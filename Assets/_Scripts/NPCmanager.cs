a0b8c6ba2e75b6106dd4ffab466e497552a2149b
ywan3223 <wwwinniewang@gmail.com>
2020-03-18 13:57:04 -0400

    commit with sunnyland

---
+++ Assets/_Scripts/NPCmanager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCmanager : MonoBehaviour
+{
+    public GameObject DialogImage;
+    public float showTime = 4;
+    public float showTimer;
+
+    //initialization
+    void Start()
+    {
+        DialogImage.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        showTimer -= Time.deltaTime;
+
+        if (showTimer < 0)
+        {
+            DialogImage.SetActive(false);
+        }
+    }
+
+    public void ShowDialog()
+    {
+        showTimer = showTime;
+        DialogImage.SetActive(true);
+    }
+}
