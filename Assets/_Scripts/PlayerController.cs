fab1aeb1a331a2c8352a99453ef248dd6acf98f1
ZisongZhao <59808723+ZisongZhao@users.noreply.github.com>
2020-03-18 17:58:02 -0400

    jagger commit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private Vector2 lookDirection = new Vector2(1, 0);//default
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        Vector2 moveVector = new Vector2(moveH, moveV);
+
+        if(moveVector.x !=0 || moveVector.y != 0)
+        {
+            lookDirection = moveVector;
+        }
+
+        //anim
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rb.position, lookDirection, 2f, LayerMask.GetMask("NPC"));
+            if(hit.collider != null) {
+                NPCmanager npc = hit.collider.GetComponent<NPCmanager>();
+
+                if(npc != null){
+                    npc.ShowDialog();
+                }
+            }
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+       
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
---
+++ Assets/_Scripts/collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController pc = other.GetComponent<PlayerController>();
+        if (pc != null)
+        {
+            Debug.Log("the player touch the stawberry");
+        }
+    }
+}
