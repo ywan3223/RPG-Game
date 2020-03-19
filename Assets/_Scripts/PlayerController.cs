5910c10423443e88c4c39ff95c320ade281ce9f2
ZisongZhao <59808723+ZisongZhao@users.noreply.github.com>
2020-03-19 04:01:06 -0400

    zisong commit

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,22 +7,33 @@ public class PlayerController : MonoBehaviour
     public static PlayerController instance;
 
     private Rigidbody2D rb;
-    private float moveH, moveV;
-    [SerializeField] private float moveSpeed = 5.0f;
-    private int maxHealth = 5;
-    private int currentHealth;
     
-   private Vector2 lookDirection = new Vector2(1, 0);//default
-    public GameObject bulletPrefab;
+    private float moveH, moveV;
+    private bool isJumpPressed = false;
+    
 
-    void Start()
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private Vector2 lookDirection = new Vector2(1, 0);//default
+
+    private void Start()
     {
-        
         rb = GetComponent<Rigidbody2D>();
+        if (isJumpPressed)
+        {
+            // the cube is going to move upwards in 10 units per second
+
+            
+            rb.velocity = new Vector2(-70, 30);
+            Debug.Log("jump");
+        }
     }
 
-    void Update()
+    private void Update()
     {
+        
         moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
         moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
 
@@ -34,38 +44,45 @@ public class PlayerController : MonoBehaviour
             lookDirection = moveVector;
         }
 
-        //anim
+        isJumpPressed = Input.GetButtonDown("Jump");
+        
+
+     
+
+
+
+
+
+
+
 
         if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit2D hit = Physics2D.Raycast(rb.position, lookDirection, 2f, LayerMask.GetMask("NPC"));
-            if (hit.collider != null) {
+            if (hit.collider != null)
+            {
                 NPCmanager npc = hit.collider.GetComponent<NPCmanager>();
 
-                if (npc != null) {
+                if (npc != null)
+                {
                     npc.ShowDialog();
                 }
             }
 
         }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            GameObject bullet = Instantiate(bulletPrefab, rb.position, Quaternion.identity);
-            bulletcontroller bc = bullet.GetComponent<bulletcontroller>();
-            if (bc != null)
-            {
-                bc.Move(lookDirection, 100);
-            }
-        }
     }
 
-    
-   
-
-
-    public void FixedUpdate()
+    private void FixedUpdate()
     {
-       
+
         rb.velocity = new Vector2(moveH, moveV);
+
+        if (isJumpPressed)
+        {
+            // the cube is going to move upwards in 10 units per second
+            rb.velocity = new Vector2(-100, 0);
+           
+            Debug.Log("jump");
+        }
     }
 }
