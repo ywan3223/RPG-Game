a1d8c4263f60fe4b84781d59505781be17e25519
yzha2776 <62304919+yzha2776@users.noreply.github.com>
2020-03-19 03:54:21 -0400

    Yuxin committ

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,22 +10,26 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float moveH, moveV;
     [SerializeField] private float moveSpeed = 5.0f;
+    private int maxHealth = 5;
+    private int currentHealth;
+    
+   private Vector2 lookDirection = new Vector2(1, 0);//default
+    public GameObject bulletPrefab;
 
-    private Vector2 lookDirection = new Vector2(1, 0);//default
-
-    private void Start()
+    void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    void Update()
     {
         moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
         moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
 
         Vector2 moveVector = new Vector2(moveH, moveV);
 
-        if(moveVector.x !=0 || moveVector.y != 0)
+        if (moveVector.x != 0 || moveVector.y != 0)
         {
             lookDirection = moveVector;
         }
@@ -34,18 +39,31 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit2D hit = Physics2D.Raycast(rb.position, lookDirection, 2f, LayerMask.GetMask("NPC"));
-            if(hit.collider != null) {
+            if (hit.collider != null) {
                 NPCmanager npc = hit.collider.GetComponent<NPCmanager>();
 
-                if(npc != null){
+                if (npc != null) {
                     npc.ShowDialog();
                 }
             }
 
         }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, rb.position, Quaternion.identity);
+            bulletcontroller bc = bullet.GetComponent<bulletcontroller>();
+            if (bc != null)
+            {
+                bc.Move(lookDirection, 100);
+            }
+        }
     }
 
-    private void FixedUpdate()
+    
+   
+
+
+    public void FixedUpdate()
     {
        
         rb.velocity = new Vector2(moveH, moveV);
---
+++ Assets/_Scripts/bulletcontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletcontroller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(this.gameObject, 2f);
+    }
+     void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Move(Vector2 moveDirection, float moveForce)
+    {
+        rb.AddForce(moveDirection * moveForce);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+
+    {
+        Destroy(this.gameObject);  
+    }
+}
